0c4b63d30f055ce9ca345a1367a3dead6b01a4bb
raatmarien <raatmarien@users.noreply.github.com>
2015-04-25 22:24:52 +0200

    Add name generator

--- perfectfighter.cpp
+++ perfectfighter.cpp
@@ -1,15 +1,18 @@
 #include <SFML/Graphics.hpp>
 #include <iostream>
+#include <string>
 
 using namespace std;
 
 bool isMax(int min, int max, int randint);
 bool isPerfect(int randint);
+string genName(int seed);
+string toString(int num);
 
 int main () {
     int seed;
     for (seed = 0; !isPerfect(seed); seed++);
-    cout << seed << endl;
+    cout << genName(seed) << endl;
 }
 
 bool isMax(int min, int max, int randint) {
@@ -26,4 +29,53 @@ bool isPerfect(int seed) {
         && isMax(5, 20, randint) // strength
         && isMax(12, 28, randint) // jump
         && isMax(75, 120, randint);
+}
+
+string genName(int seed) {
+    int rest = -1;
+    string name = "a";
+    bool done = false;
+    for (int i = 32; i < 127 && !done; i++) {
+        for (int j = 32; j < 127; j++) {
+            int left = seed - ((i * 4) * j);
+            if (left > 0 && left < 126 * 4) {
+                rest = left;
+                for (int x = 0; x < 4; x++) {
+                    name += (char) i;
+                }
+                name += (char) j;
+                done = true;
+                break;
+            }
+        }
+    }
+    if (rest != -1) {
+        for (int i = 0; i < 4; i++) {
+            int charNum = rest / (4 - i);
+            name += (char) charNum;
+            rest -= rest / (4 - i);
+        }
+        name += (char) rest;
+        string msg = "The seed is ";
+        msg.append(toString(seed));
+        msg.append(". The name found is ");
+        msg.append(name);
+        return msg;
+    } else {
+        name = "No name could be found, the seed is ";
+        name.append(toString(seed));
+    }
+    return name;
+}
+
+
+string toString(int num) {
+    string numString;
+    while(num > 0) {
+        numString = ((char) ((num % 10)
+                      + 48))
+            + numString;
+        num /= 10;
+    }
+    return numString;
 }
